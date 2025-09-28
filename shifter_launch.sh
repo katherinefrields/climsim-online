6423e69f27e4227967da3d376c0b3e736405d5b9
Katherine Frields <kfrields@login39.chn.perlmutter.nersc.gov>
2025-09-27 19:23:12 -0700

    working repo

--- shifter_launch.sh
+++ shifter_launch.sh
@@ -6,16 +6,15 @@
 #SBATCH -J climsim
 #SBATCH -A m4334
 #SBATCH --mail-user=frieldskatherine@gmail.com
+#SBATCH --mail-type=BEGIN,END,FAIL
 #SBATCH --gpus-per-node=1
 
-# Paths on NERSC (make sure these dirs exist in your $HOME or $SCRATCH)
-INPUTDATA=/pscratch/sd/k/kfrields/climsim-online-data/inputdata
+# Paths on NERSC (make sure these dirs exist in your $HOME or $SCRATCH
 SHARED=/global/homes/k/kfrields/shared_e3sm
-SCRATCH_DIR=/pscratch/sd/k/kfrields/climsim-online-data/scratch
 
 
 srun -n 1 shifter --image=docker:katherinefrields/e3sm-climsim:latest \
-    --volume=$INPUTDATA:/storage/inputdata \
-    --volume=$SHARED:/storage/shared_e3sm \
-    --volume=$SCRATCH_DIR:/scratch \
-    python example_job_submit_mmf.py
+    --volume=/pscratch/sd/k/kfrields/climsim-online-data/inputdata:/storage/inputdata \
+    --volume=/pscratch/sd/k/kfrields/climsim-online-data/scratch:/scratch \
+    python /global/homes/k/kfrields/climsim-online/E3SM/climsim_scripts/example_job_submit_mmf.py
+
