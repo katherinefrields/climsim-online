98866c4e30f9d2191792476d75cad4f8e3c2013d
Katherine Frields <119017028+katherinefrields@users.noreply.github.com>
2026-03-03 01:07:16 -0800

    update shared e3sm location

--- shifter_launch.sh
+++ shifter_launch.sh
@@ -10,13 +10,12 @@
 #SBATCH --gpus-per-node=1
 
 # Paths on NERSC (make sure these dirs exist in your $HOME or $SCRATCH
-SHARED=/global/homes/k/kfrields/shared_e3sm
+SHARED=/pscratch/sd/k/kfrields/climsim-online-data/shared_e3sm
 
 
 srun -n 1 shifter --image=docker:katherinefrields/e3sm-climsim:latest \
     --volume=/pscratch/sd/k/kfrields/climsim-online-data/inputdata:/storage/inputdata \
     --volume=/pscratch/sd/k/kfrields/climsim-online-data/scratch:/scratch \
-
     python /global/homes/k/kfrields/climsim-online/E3SM/climsim_scripts/example_job_submit_mmf.py
 
 #python /global/homes/k/kfrields/climsim-online/E3SM/climsim_scripts/diff_mmf.py
